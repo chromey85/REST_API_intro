f6d9e26d8e306acf5c35d4248fe3db7a1b42e38e
chromey85 <yayyub85@gmail.com>
2022-03-08 16:55:37 +0000

    edits

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const { jwt } = require("jsonwebtoken");
+const User = require("../user/userModel");
 
 exports.hashPassword = async (req, res, next) => {
   try {
@@ -10,5 +12,42 @@ exports.hashPassword = async (req, res, next) => {
   } catch (error) {
     console.log(error);
     res.status(500).send({ err: error.message });
+  }
+};
+
+exports.decryptPassword = async (req, res, next) => {
+  try {
+    req.user = await User.findOne({ username: req.body.username });
+    if (await bcrypt.compare(req.body.password, req.user.password)) {
+      next();
+    } else {
+      throw new Error("incorrect Credentials");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error.message });
+  }
+};
+
+exports.checkToken = async (req, res, next) => {
+  try {
+    // const authHeader = req.header("Authorization");
+    // const token = authHeader.replace("Bearer ", "");
+    const authHeader = req.header("Authorization").replace("Bearer ", "");
+    const decodedToken = await jwt.verify(token, process.env.SECRET);
+    const user = await User.findById(decodedToken._id);
+    if (req.user) {
+      next();
+    } else {
+      throw new Error("No User Found");
+    }
+    console.log(authHeader);
+    console.log(token);
+    console.log(decodedToken);
+    console.log(user);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error.log });
   }
 };
--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
-const { addUser, deleteUser, editUser } = require("./userController");
-const { hashPassword } = require("../middleware");
+const { addUser, login, deleteUser, editUser } = require("./userController");
+const { hashPassword, decryptPassword, checkToken } = require("../middleware");
 const userRouter = Router();
 
 userRouter.post("/user", hashPassword, addUser);
-// userRouter.delete("/user", hashPassword, addUser);
+userRouter.post("/login", decryptPassword, login);
+userRouter.get("/user", checkToken, login);
+
+// userRouter.delete("/user", hashPassword, deleteUser);
 // userRouter.patch("/user", hashPassword, addUser);
 
 module.exports = userRouter;
